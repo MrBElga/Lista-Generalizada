41a537e03e7ac3e8dadbe880a406614b416942a9
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2023-09-10 19:47:21 -0300

    Update Fila

--- tad.h
+++ tad.h
@@ -1,3 +1,9 @@
+struct Fila{
+    int info;
+    struct Fila *prox;
+};
+typedef struct Fila fila;
+
 struct reg_lista
 {
     struct listagen *cabeca;
@@ -144,4 +150,62 @@ char comparaListaGen(ListaGen *L, ListaGen *L2)
     if(Atomo(L) || Atomo(L2))
          return 0; //0
     return comparaListaGen(Head(L), Head(L2)) && comparaListaGen(Tail(L), Tail(L2));
+}
+
+
+void init(fila **inicio){
+        *inicio = NULL;
+}
+
+char QisEmpety(fila *inicio){
+	return inicio == NULL;
+}
+
+void Enqueue(fila **inicio, int info){
+    fila *nova = (fila*) malloc(sizeof(fila)), *aux = *inicio;
+    nova->info = info;
+    nova->prox = NULL;
+    if(!QisEmpety(*inicio)){
+       while(aux->prox !=NULL)
+        {
+            aux = aux -> prox;
+        }
+        aux->prox = nova;
+    }
+    else
+    {
+    	 *inicio = nova;
+        
+    }
+}
+
+void Dequeue(fila **inicio, int *info ){
+    fila *aux = *inicio;
+    if(!QisEmpety(*inicio)){      
+		*info = (*inicio)->info;
+		*inicio = (*inicio)->prox;
+		free(aux);	
+	}
+	else{
+		*info = -1;
+	}
+}
+ 
+int top(fila *inicio){
+    return inicio->info;    
+}
+
+void exibe(fila *inicio){
+    system("cls");
+    if(inicio == NULL){
+    	printf("FILA: vazia");
+	}
+	else{
+		printf("FILA: ");
+	    while(inicio != NULL){
+	        printf("%d ", inicio->info);
+	        inicio=inicio->prox;
+	    }
+	}
+	printf("\n");
 }
